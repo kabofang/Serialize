eaae496d35c37cfeae9962314fc6f1459aa38275
kabofang <fangkb555@gmail>
2021-05-12 03:52:01 +0800

    create v3

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include <fcntl.h>
 #include <vector>
 using namespace std;
+struct StObj{
+	int m_Type;
+	void* m_pObj;
+};
 class CA{
 private:
 	int m_i;
@@ -31,24 +35,65 @@ bool CA::DeSerialize(int fd){
 	return true;
 }
 
-class CSerializer{
-public://问老师static与之区别
-	bool Serialize(const char* pFilePath , const vector<CA> &Vec) const;
-	bool DeSerialize(const char* pFilePath , vector<CA> &Vec);
+class CB{
+private:
+	double m_d;
+public:
+	CB() { m_d=0; }
+	explicit CB(double x) : m_d(x) {}
+
+	void SetData(double x) {m_d = x;}
+	double GetData(void) { return m_d; }
+
+	bool Serialize(int fd) const;
+	bool DeSerialize(int fd);
 };
 
-bool CSerializer::Serialize(const char* pFilePath , const vector<CA> &Vec) const{
+bool CB::Serialize(int fd) const{
+	if(-1 == ::write(fd,&m_d,sizeof(m_d))){
+		cout<<"Write file fail!"<<endl;
+		return false;
+	}
+	return true;
+}
+bool CB::DeSerialize(int fd){
+	int ret = ::read(fd,&m_d,sizeof(m_d));
+	if(-1 == ret || 0 == ret)
+		return false;
+	return true;
+}
+
+
+class CSerializer{
+public://问老师static与之区别
+	bool Serialize(const char* pFilePath , const vector<StObj> &Vec) const;
+	bool DeSerialize(const char* pFilePath , vector<StObj> &Vec);
+};
+
+bool CSerializer::Serialize(const char* pFilePath , const vector<StObj> &Vec) const{
 	int fd;
 	if(-1 == (fd = ::open(pFilePath,O_CREAT|O_TRUNC|O_WRONLY,00600))){
 		cout << "Open file fail!" << endl;
 		return false;
 	}
-
-	for(int i = 0 ; i < Vec.size() ; i++)
-		if(!Vec[i].Serialize(fd)){
-			cout << "Serialize fail!" << endl;
+	for(int i = 0 ; i < Vec.size() ; i++){
+		if(-1 == ::write(fd,&(Vec[i].m_Type),sizeof(Vec[i].m_Type))){
+			cout << "Write file fail!" << endl;
 			return false;
 		}
+
+		if(1 == Vec[i].m_Type)
+			if( !(((CA*)(Vec[i].m_pObj))->Serialize(fd)) ){
+				cout << "Serialize fail!" << endl;
+				return false;
+			}	
+
+		if(2 == Vec[i].m_Type)
+			if( !(((CB*)(Vec[i].m_pObj))->Serialize(fd)) ){
+				cout << "Serialize fail!" << endl;
+				return false;
+			}
+	}
 
 	if(-1 == ::close(fd)){
 		cout << "Close file fail!" << endl;
@@ -56,7 +101,7 @@ bool CSerializer::Serialize(const char* pFilePath , const vector<CA> &Vec) const
 	}
 	return true;
 }
-bool CSerializer::DeSerialize(const char* pFilePath , vector<CA> &Vec){	
+bool CSerializer::DeSerialize(const char* pFilePath , vector<StObj> &Vec){	
 	int fd;
 	if(-1 == (fd = ::open(pFilePath,O_RDONLY))){
 		cout << "Open file fail!" << endl;
@@ -64,9 +109,29 @@ bool CSerializer::DeSerialize(const char* pFilePath , vector<CA> &Vec){
 	}
 
 	for(;;){
-		CA Temp;
-		if(!Temp.DeSerialize(fd))//失败与文件尾的分支？
+		StObj Temp;
+
+		int ret;
+		ret = ::read(fd,&Temp.m_Type,sizeof(Temp.m_Type));
+		if(-1 == ret || 0 == ret)//读出错与文件结尾分支
 			break;
+
+		if(1 == Temp.m_Type){
+			Temp.m_pObj = new CA;
+			if( !(((CA*)(Temp.m_pObj))->DeSerialize(fd)) ){
+				cout << "DeSerialize fail!" << endl;
+				return false;
+			}	
+		}
+
+		if(2 == Temp.m_Type){
+			Temp.m_pObj = new CB;
+			if( !(((CB*)(Temp.m_pObj))->DeSerialize(fd)) ){
+				cout << "bDeSerialize fail!" << endl;
+				return false;
+			}	
+		}
+
 		Vec.push_back(Temp);
 	}
 
@@ -74,27 +139,38 @@ bool CSerializer::DeSerialize(const char* pFilePath , vector<CA> &Vec){
 		cout << "Close file fail!" << endl;
 		return false;
 	}
+
 	return true;
 }
 
 
 int main(int argc , char* argv[]){
-	CA a[10];
-	vector<CA> VecA;
-	vector<CA> Vecdst;
+	CA a1(5),a2(10);
+	CB b1(1.25),b2(2.5);
+	StObj st1 = {.m_Type = 1,.m_pObj = &a1};
+	StObj st2 = {.m_Type = 2,.m_pObj = &b1};
+	StObj st3 = {.m_Type = 1,.m_pObj = &a2};
+	StObj st4 = {.m_Type = 2,.m_pObj = &b2};
 
-	for(int i = 0 ; i < 10 ; i++){
-		a[i].SetData(2*i);
-		VecA.push_back(a[i]);
-	}
+	vector<StObj> VecSrc;
+	vector<StObj> Vecdst;
+
+	VecSrc.push_back(st1);
+	VecSrc.push_back(st2);
+	VecSrc.push_back(st3);
+	VecSrc.push_back(st4);
 
 	CSerializer Ser;
 
-	Ser.Serialize("a.txt",VecA);
+	Ser.Serialize("a.txt",VecSrc);
 	Ser.DeSerialize("a.txt",Vecdst);
-
-	for(int i = 0 ; i < Vecdst.size() ; i++)
-		cout<<Vecdst[i].GetData()<<endl;
+	
+	for(int i = 0; i < Vecdst.size() ; i++){
+		if(1 == Vecdst[i].m_Type)
+			cout <<((CA*)(Vecdst[i].m_pObj))->GetData() << endl;
+		if(2 == Vecdst[i].m_Type)
+			cout <<((CB*)(Vecdst[i].m_pObj))->GetData() << endl;
+	}
 
 	return 0;
 }
